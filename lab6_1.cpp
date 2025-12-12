7d6baf8a74eb64f285cfe8391e4f7ffcf7c131ba
NostalgiaX-coder <iam01sweetz@gmail.com>
2025-12-12 13:27:09 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int n = -1 ; 
+    int evenCount = 0;
+    int oddCount = 0;
+
+    while (n != 0) { 
+        cout << "Enter an integer: ";
+        cin >> n;
+
+        if (n != 0) {  
+            if (n % 2 == 0)
+                evenCount++;
+            else
+                oddCount++;
+        }
+    }
+
+    cout << "#Even numbers = " << evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
+
     return 0;
 }
